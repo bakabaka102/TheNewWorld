b81f2c076eb10bafc57da13536d1a6c99148cc55
HanNgg <HanNgg>
2025-12-09 23:58:53 +0700

    Add start scene, main menu, and player controls
    - Created start scene with buttons for Start, Exit, and Options.
    - Implemented functionality for buttons in start.gd script.
    - Added main menu scene structure.
    - Established player movement and jump mechanics in player.gd.
    - Included necessary directories and .gitkeep files for organization.

---
+++ scripts/player/player.gd
@@ -0,0 +1,85 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+const MAX_JUMP = 6
+
+var jump_count = 0
+var is_double_jumping = false   # Lock animation doublejump
+
+var audio_jump_path = load("res://assets/audio/jump.wav")
+
+@onready var anim = $AnimatedSprite2DPlayer
+@onready var audio_jump = $AudioStreamPlayer_Jump
+
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("ui_left", "ui_right")
+
+	# --------------------------------------------------------
+	#  multi-jump + doublejump animation
+	# --------------------------------------------------------
+	if Input.is_action_just_pressed("ui_accept"):
+		# Nhảy được nếu đang đứng đất hoặc còn số lần nhảy
+		if is_on_floor() or jump_count < MAX_JUMP:
+			velocity.y = JUMP_VELOCITY
+			jump_count += 1
+
+			# Lần nhảy đầu
+			if jump_count == 1:
+				anim.play("jump")
+				is_double_jumping = false
+			
+			# Từ lần 2 trở đi (double jump)
+			else:
+				anim.play("doublejump")
+				is_double_jumping = true
+
+			# Play âm thanh
+			audio_jump.stream = audio_jump_path
+			audio_jump.play()
+
+	# ---------------------------------------
+	#  ANIMATION fly (jump/fall logic)
+	# ---------------------------------------
+	if not is_on_floor():
+
+		# Không override animation doublejump
+		if is_double_jumping:
+			velocity += get_gravity() * delta
+			# vẫn rơi bình thường nhưng không đổi animation
+			pass
+		else:
+			velocity += get_gravity() * delta
+			
+			if velocity.y < 0:
+				anim.play("jump")
+			else:
+				anim.play("fall")
+	else:
+		# Reset số lần nhảy khi chạm đất
+		if jump_count != 0:
+			jump_count = 0
+			is_double_jumping = false
+
+		# Animation khi đứng đất
+		if direction != 0:
+			anim.play("run")
+		else:
+			anim.play("idle")
+
+	# ---------------------------------------
+	#  MOVEMENT
+	# ---------------------------------------
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Flip sprite
+	if direction < 0.0:
+		anim.flip_h = true
+	elif direction > 0.0:
+		anim.flip_h = false
+
+	move_and_slide()
