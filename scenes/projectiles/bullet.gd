27c2693086b31405d0afd7c13c30e07c46b81713
HanNgg <HanNgg>
2025-12-16 00:23:36 +0700

    feat: Update player collision settings, enhance bullet damage handling, and remove unused hit effect resources

--- scenes/projectiles/bullet.gd
+++ scenes/projectiles/bullet.gd
@@ -3,8 +3,12 @@ extends Area2D
 @export var speed := 300.0
 var direction := Vector2.RIGHT
 
+@export var damage := 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# đảm bảo signal được connect
+	body_entered.connect(_on_body_entered)
 	pass # Replace with function body.
 
 
@@ -14,7 +18,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body):
-	if body.is_in_group("enemy_bee"):
+	if body.is_in_group("enemy"):
 		print_debug("body.hit()")
-		body.hit()
+		body.hit(damage)
 		#explode()
--- ui/effects/hit_effect.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	$AnimationPlayer.play("hit")
-
-func _on_animation_finished(_name):
-	queue_free()
